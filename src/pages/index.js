dce784a6f7d4e257bcb56db4f9ea786104ebc36c
Marcin Stanek <marcin.stanek@elexon.co.uk>
2021-12-10 17:02:55 +0100

    initial blog list

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,14 +4,17 @@ import { graphql } from "gatsby"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 import { Jumbotron } from "../components/home/jumbotron"
+import { LastBlogPosts } from "../components/home/lastblogbosts"
 
 const BlogIndex = ({ data, location }) => {
   const siteTitle = data.site.siteMetadata?.title || `Title`
+  const posts = data.allMarkdownRemark.nodes.filter(post => post.frontmatter.language === "pl")
 
   return (
     <Layout location={location} title={siteTitle}>
       <Seo title="Strona główna" />
       <Jumbotron />
+      <LastBlogPosts name={"Marcin"} posts={posts}/>
     </Layout>
   )
 }
@@ -23,6 +26,20 @@ export const pageQuery = graphql`
     site {
       siteMetadata {
         title
+      }
+    }
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+      nodes {
+        excerpt
+        fields {
+          slug
+        }
+        frontmatter {
+          date(formatString: "MMMM DD, YYYY")
+          title
+          description
+          language
+        }
       }
     }
   }
