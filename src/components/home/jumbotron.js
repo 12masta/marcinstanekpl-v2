970b9ac1b0ee9863dce9c457be192c1209f57211
Marcin Stanek <marcin.stanek@elexon.co.uk>
2021-12-22 15:36:40 +0100

    jumbotron adjustments

--- src/components/home/jumbotron.js
+++ src/components/home/jumbotron.js
@@ -15,12 +15,10 @@ export class Jumbotron extends React.Component {
           />
         </div>
         <div className="col-lg-6">
-          <h1 className="display-5 fw-bold lh-1 mb-3">Inicjuję i naprawiam procesy kontroli jakości oprogramowania</h1>
-          <p className="lead">Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s
-            most popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid system,
-            extensive prebuilt components, and powerful JavaScript plugins.</p>
+          <h1 className="display-5 fw-bold lh-1 mb-3">{this.props.header}</h1>
+          <p className="lead">{this.props.description}</p>
           <div className="d-grid gap-2 d-md-flex justify-content-md-start">
-            <button type="button" className="btn btn-primary btn-lg px-4 me-md-2">Chcę wiedzieć więcej</button>
+            <button type="button" className="btn btn-primary btn-lg px-4 me-md-2">{this.props.wantToKnowMoreButtonText}</button>
             <button href="/blog/pl/" type="button" className="btn btn-outline-secondary btn-lg px-4">Blog</button>
           </div>
         </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -15,7 +15,9 @@ const BlogIndex = ({ data, location }) => {
   return (
     <Layout location={location} title={siteTitle}>
       <Seo title="Strona główna"/>
-      <Jumbotron/>
+      <Jumbotron header={"Inicjuję i naprawiam procesy kontroli jakości oprogramowania"}
+                 description={"Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s most popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid system, extensive prebuilt components, and powerful JavaScript plugins."}
+                 wantToKnowMoreButtonText={"Chcę wiedzieć więcej"}/>
       <LastBlogPosts header={"Ostatnie wpisy na blogu"} posts={posts}/>
       <Featurette header={"Quality Assurance"}
                   leadText={"Poszukujesz kontrolera jakości? Potrzebujesz zautomatyzować procesy testowe w Twojej firmie? Masz problem z jakością kodu w Twojej organizacji? Trafiłeś w dobre miejsce, aby rozwiązać te problemy!"}
@@ -34,7 +36,8 @@ const BlogIndex = ({ data, location }) => {
                   image={"inform"} alignToLeft={true}/>
       <ContactForm header={"Kontakt"} labelMessage={"Wiadomość"}
                    messageCheckboxText={"Dodam Cię do listy mailowej, z której możesz wypisać się w dowolnym momencie (jeden klik)"}
-                   privacyPolicyText={"Polityka Prywatności"} buttonText={"Wyślij"} privacyPolicyLink={"/regulamin.html"}/>
+                   privacyPolicyText={"Polityka Prywatności"} buttonText={"Wyślij"}
+                   privacyPolicyLink={"/regulamin.html"}/>
     </Layout>
   )
 }
