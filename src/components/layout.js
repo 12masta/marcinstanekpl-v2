23e4ede8298de1ef39a0916d4d49c11393ffc3b7
Marcin Stanek <marcin.stanek@elexon.co.uk>
2022-02-10 11:16:07 +0100

    cookie consent first approach

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,5 +1,6 @@
 import * as React from "react"
 import { Nav } from "./nav"
+import CookieConsent from "react-cookie-consent"
 
 const Layout = ({ location, title, children }) => {
   const rootPath = `${__PATH_PREFIX__}/`
@@ -8,14 +9,24 @@ const Layout = ({ location, title, children }) => {
   return (
     <div className="global-wrapper" data-is-root-path={isRootPath}>
       <main role="main">
-        <Nav pathName={location.pathname}/>
+        <Nav pathName={location.pathname} />
         <div className="container">{children}</div>
       </main>
 
+      <CookieConsent
+        location="bottom"
+        buttonText="Akceptuj"
+        declineButtonText="Decline"
+        cookieName="gatsby-gdpr-google-analytics"
+      >
+        Ta witryna przechowuje dane, w tym m.in. pliki cookie, aby zapewnić
+        dostęp do podstawowych funkcjonalności witryny, a także na potrzeby
+        marketingu, personalizacji i analiz. W każdej chwili możesz zmienić
+        swoje ustawienia lub zaakceptować ustawienia domyślne. Polityka Cookie
+      </CookieConsent>
+
       <div className="container">
-      <footer>
-        © marcinstanek.pl, {new Date().getFullYear()}
-      </footer>
+        <footer>© marcinstanek.pl, {new Date().getFullYear()}</footer>
       </div>
     </div>
   )
