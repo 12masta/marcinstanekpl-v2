1b20edd073337d16ee26118ee08c3ce163e82716
Marcin Stanek <marcin.stanek@elexon.co.uk>
2022-03-25 20:05:36 +0100

    Google Analytics key

--- gatsby-config.js
+++ gatsby-config.js
@@ -56,12 +56,12 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
-    // {
-    //   resolve: `gatsby-plugin-google-analytics`,
-    //   options: {
-    //     trackingId: `ADD YOUR TRACKING ID HERE`,
-    //   },
-    // },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: `UA-119547384-2`,
+      },
+    },
     {
       resolve: `gatsby-plugin-feed`,
       options: {
