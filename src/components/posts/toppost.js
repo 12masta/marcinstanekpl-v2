d03390912a43db8d4c00294b94cd19a1bb6b0fc7
12masta <stanek.marcinp@gmail.com>
2026-03-29 22:34:00 +0200

    Refactor FeaturedPost and TopPost components to use Gatsby's Link component
    - Updated FeaturedPost and TopPost components to replace anchor tags with Gatsby's Link component for improved navigation.
    - Simplified prop destructuring for better readability and maintainability.
    - Enhanced layout consistency by adjusting class names and structure.

--- src/components/posts/toppost.js
+++ src/components/posts/toppost.js
@@ -1,14 +1,28 @@
 import * as React from "react"
+import { Link } from "gatsby"
 
 export class TopPost extends React.Component {
   render() {
+    const { post, label } = this.props
+    const slug = post.fields.slug
+
     return (
-      <div className="p-4 p-md-5 mb-4 rounded text-body-emphasis bg-body-secondary">
+      <div className="p-4 p-md-5 mb-4 rounded text-body-emphasis bg-body-secondary position-relative">
         <div className="col-lg-12 px-0">
-          <h1 className="display-4">{this.props.post.frontmatter.title}</h1>
-          <p className="lead my-3">{this.props.post.frontmatter.description || this.props.post.excerpt}</p>
-          <p className="lead mb-0"><a href={this.props.post.fields.slug} className="text-body-emphasis fw-bold">{this.props.label}</a></p>
+          <h1 className="display-4">{post.frontmatter.title}</h1>
+          <p className="lead my-3">
+            {post.frontmatter.description || post.excerpt}
+          </p>
+          <p className="lead mb-0">
+            <Link
+              to={slug}
+              className="fw-bold stretched-link text-body-emphasis icon-link icon-link-hover"
+            >
+              {label}
+            </Link>
+          </p>
         </div>
-      </div>)
+      </div>
+    )
   }
 }
